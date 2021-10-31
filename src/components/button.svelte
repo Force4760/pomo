222ac2083902ad5aa0ef3cd6be0ad365427b82bd
Force4760 <gforce4760@gmail.com>
2021-10-31 19:02:44 +0000

    fix modal

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -1,27 +1,35 @@
 <script lang="ts">
-    export let title: String = "Settings";
-    export let mode: String = "short"
+  export let title: String = "Settings";
+  export let mode: String = "short";
 </script>
 
 {#if mode === "work"}
-    <a href="/components/modal#my-modal" class="btn btn-down btn-primary">{title}</a>
+  <label for="my-modal-2" class="btn btn-down btn-primary modal-button"
+    >{title}</label
+  >
+  <input type="checkbox" id="my-modal-2" class="modal-toggle" />
 {:else if mode === "short"}
-    <a href="/components/modal#my-modal" class="btn btn-down btn-accent">{title}</a>
+  <label for="my-modal-2" class="btn btn-down btn-accent modal-button"
+    >{title}</label
+  >
+  <input type="checkbox" id="my-modal-2" class="modal-toggle" />
 {:else}
-    <a href="/components/modal#my-modal" class="btn btn-down btn-secondary">{title}</a>
+  <label for="my-modal-2" class="btn btn-down btn-secondary modal-button"
+    >{title}</label
+  >
+  <input type="checkbox" id="my-modal-2" class="modal-toggle" />
 {/if}
 
 <style>
-    .btn-down {
-      position: absolute;
-      bottom: 5%;
-      left: 50%;
-      translate: -50%;
-    }
-  
-    .btn-down:focus {
-      transform: none;
-    }
+  .btn-down {
+    position: absolute;
+    bottom: 5%;
+    left: 50%;
+    translate: -50%;
+  }
+
+  .btn-down:focus {
+    transform: none;
+  }
 </style>
-  
-    
+
--- src/components/modal.svelte
+++ src/components/modal.svelte
@@ -11,19 +11,29 @@
   function reset() {
     $work = 25;
     $audio = true;
-    $distraction = false;
+    $distraction = true;
     $shortT = 5;
     $longT = 15;
     $toLongT = 4;
   }
 </script>
 
-<div id="my-modal" class="modal">
+<div class="modal">
   <div class="modal-box">
     <slot />
     <div class="modal-action">
-      <div class="btn btn-primary" on:click={reset}>Reset</div>
-      <a href="/components/modal#" class="btn">Close</a>
+      <label for="" class="btn btn-primary" on:click={reset}>Reset</label>
+      <label for="my-modal-2" class="btn">Close</label>
     </div>
   </div>
 </div>
+
+<style>
+  .modal-toggle {
+    position: fixed;
+    bottom: 10px;
+    left: 50%;
+    transform: translate(-50%, 0%);
+    z-index: 20;
+  }
+</style>
